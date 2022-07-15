7b10a2c40075d570ec37e9306e9d261da22917f4
Tom Elliott <tomelliottnz@gmail.com>
2022-07-15 16:53:00 +1200

    about page, icons, etc

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import About from "./About";
 
 import LoadData from "./components/data/LoadData";
 import ViewData from "./components/data/ViewData";
@@ -10,50 +12,25 @@ import DisclosureOptions from "./components/DisclosureOptions";
 import DisclosureResults from "./components/DisclosureResults";
 import { AnimatePresence, AnimateSharedLayout, motion } from "framer-motion";
 
+
 function App() {
-  const [data, setData] = useState(null);
-  const [config, setConfig] = useState({ vars: [] });
 
   return (
     // a side-by-side for development
     <div className="App flex flex-col bg-black gap-1 min-h-screen justify-center">
 
-      <Header />
-
       <AnimateSharedLayout>
         <AnimatePresence>
-          <div className="main flex-1 flex flex-col items-stretch gap-10 bg-gradient-to-br from-gray-50 to-green-50 p-4">
+          <BrowserRouter>
+            <Header />
 
-            <div className="flex flex-col gap-10 items-center 2xl:flex-row 2xl:items-start 2xl:justify-center">
-              <div className="w-full 2xl:max-w-[40vw] flex flex-col gap-10 items-center">
-                <div className="container h-[360px] drop-shadow">
-                  {data ? <ViewData data={data} clear={() => setData(null)} /> : <LoadData setter={setData} />}
-                </div>
-
-                {data && (
-                  <motion.div initial={{ opacity: 0, y: -20 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    exit={{ opacity: 0, y: 20 }}
-                    transition={{ delay: 1.2 }}
-                    className="container bg-slate-50 border border-slate-300 rounded-md p-5 drop-shadow">
-                    <DisclosureOptions data={data} config={config} handler={setConfig} />
-                  </motion.div>
-                )}
-              </div>
-
-              {data && (
-                <motion.div
-                  initial={{ opacity: 0, y: -20 }}
-                  animate={{ opacity: 1, y: 0 }}
-                  exit={{ opacity: 0, y: 20 }}
-                  transition={{ delay: 1 }}
-                  className="container flex-1 flex justify-center p-5">
-                  <DisclosureResults data={data} config={config} />
-                </motion.div>
-              )}
+            <div className="main flex-1 flex flex-col items-stretch gap-10 bg-gradient-to-br from-gray-50 to-green-50 p-4">
+              <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/about" element={<About />} />
+              </Routes>
             </div>
-
-          </div>
+          </BrowserRouter>
         </AnimatePresence>
       </AnimateSharedLayout>
 
@@ -61,6 +38,42 @@ function App() {
 
     </div >
   );
+}
+
+function Main() {
+  const [data, setData] = useState(null);
+  const [config, setConfig] = useState({ vars: [] });
+
+  return (
+    <div className="flex flex-col gap-10 items-center 2xl:flex-row 2xl:items-start 2xl:justify-center">
+      <div className="w-full 2xl:max-w-[40vw] flex flex-col gap-10 items-center">
+        <div className="container h-[360px] drop-shadow">
+          {data ? <ViewData data={data} clear={() => setData(null)} /> : <LoadData setter={setData} />}
+        </div>
+
+        {data && (
+          <motion.div initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 20 }}
+            transition={{ delay: 1.2 }}
+            className="container bg-slate-50 border border-slate-300 rounded-md p-5 drop-shadow">
+            <DisclosureOptions data={data} config={config} handler={setConfig} />
+          </motion.div>
+        )}
+      </div>
+
+      {data && (
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 20 }}
+          transition={{ delay: 1 }}
+          className="container flex-1 flex justify-center p-5">
+          <DisclosureResults data={data} config={config} />
+        </motion.div>
+      )}
+    </div>
+  )
 }
 
 export default App;
--- app/src/components/header/Header.tsx
+++ app/src/components/header/Header.tsx
@@ -1,10 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <nav className="flex text-white items-center justify-between flex-wrap bg-grey-darkest p-4 bg-gradient-to-br from-gray-800 to-gray-700">
       <div className="flex items-center flex-no-shrink  mr-6">
-        <span className="text-xl font-bold pl-2">
-          Disclosure Risk Calculator
-        </span>
+        <Link to="/">
+          <div className="text-xl font-bold pl-2 flex items-center gap-4">
+            <div className="h-14 p-3 bg-white rounded-full">
+              <img src="/logo.png" className="h-full" />
+            </div>
+            <div className="flex flex-col">
+              <span className="pl-[50px] text-[40px] text-red-200 mb-[-50px] font-['Tangerine']">
+                <span className="text-[70px]">C</span>alculator
+              </span>
+              <span>Disclosure Risk</span>
+            </div>
+          </div>
+        </Link>
       </div>
 
       <div
@@ -13,9 +25,11 @@ const Header = () => {
       >
         <ul className="list-reset lg:flex justify-end flex-1 items-center">
           <li className="mr-3">
-            <span className="inline-block py-2 px-4 no-underline cursor-pointer">
-              About
-            </span>
+            <Link to="about">
+              <span className="inline-block py-2 px-4 no-underline cursor-pointer">
+                About
+              </span>
+            </Link>
           </li>
         </ul>
       </div>
