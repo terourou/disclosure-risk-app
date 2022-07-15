1c0346e92f6676965158f41bec388a278a2342a6
Tom Elliott <tomelliottnz@gmail.com>
2022-07-15 13:06:20 +1200

    add R logo, nice animations between states

--- app/src/components/footer/Footer.tsx
+++ app/src/components/footer/Footer.tsx
@@ -1,4 +1,7 @@
 import { useRserve } from "@tmelliott/react-rserve";
+import { AnimateSharedLayout, motion } from "framer-motion";
+
+import Rlogo from "./Rlogo.svg";
 
 const Footer = () => {
   const { R, connecting } = useRserve();
@@ -11,17 +14,50 @@ const Footer = () => {
         </span>
       </div>
 
-      <div className="flex items-center flex-no-shrink">
-        <span className="text-sm">
-          {R && R.running
-            ? "Connected to R at " +
-                process.env.REACT_APP_R_HOST?.replace(/^wss?:\/\//, "") ||
-              "localhost"
-            : connecting
-            ? "Connecting to R ..."
-            : "Not connected to R"}
-        </span>
-      </div>
+      <AnimateSharedLayout>
+        <div className="flex items-center flex-no-shrink">
+          <span className="text-xs">
+            {R && R.running ? (
+              <div className="flex items-center gap-2">
+                <motion.img
+                  src={Rlogo}
+                  className="h-8"
+                  layoutId="r-logo"
+                  transition={{ duration: 1 }}
+                />
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 1 }}
+                  className="flex flex-col items-center justify-center"
+                >
+                  <span className="text-slate-300">Connected to</span>
+                  {process.env.REACT_APP_R_HOST?.replace(/^wss?:\/\//, "") ||
+                    "localhost"}
+                </motion.div>
+              </div>
+            ) : connecting ? (
+              <div className="flex items-center gap-2">
+                <motion.span
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ delay: 1 }}
+                >
+                  Connecting &hellip;
+                </motion.span>
+                <motion.img
+                  src={Rlogo}
+                  className="h-8 animate-pulse"
+                  layoutId="r-logo"
+                  transition={{ duration: 1 }}
+                />
+              </div>
+            ) : (
+              "Not connected to R"
+            )}
+          </span>
+        </div>
+      </AnimateSharedLayout>
     </footer>
   );
 };
