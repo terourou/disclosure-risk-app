e17291ec54bc859cd3ef68df854b639a964bfa17
Tom Elliott <tomelliottnz@gmail.com>
2023-08-18 13:27:54 +1200

    export PORT variable

--- app/src/rserve/start.R
+++ app/src/rserve/start.R
@@ -1,4 +1,8 @@
-source('init.R')
+source("init.R")
+
+conf <- readLines("rserve.conf.template")
+conf <- gsub("$PORT", Sys.getenv("PORT", 8081), conf, fixed = TRUE)
+writeLines(conf, "rserve.conf")
 
 Rserve::run.Rserve(
     config.file = "rserve.conf"
