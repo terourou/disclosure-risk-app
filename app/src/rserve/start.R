057a6c4081d79133be8362fcf67f49dda18f9c9d
Tom Elliott <tomelliottnz@gmail.com>
2023-08-28 09:10:04 +1200

    log output

--- app/src/rserve/start.R
+++ app/src/rserve/start.R
@@ -4,6 +4,8 @@ conf <- readLines("rserve.conf.template")
 conf <- gsub("$PORT", Sys.getenv("PORT", 8081), conf, fixed = TRUE)
 writeLines(conf, "rserve.conf")
 
+cat("Starting Rserve on port", Sys.getenv("PORT", 8081), "\n")
+
 Rserve::run.Rserve(
     config.file = "rserve.conf"
     # args = c(
