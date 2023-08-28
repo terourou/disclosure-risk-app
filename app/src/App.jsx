8d5388f5c91119336eff8a626c4fd5dc9c1790d1
Tom Elliott <tomelliottnz@gmail.com>
2023-08-28 16:29:55 +1200

    tweak code

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
   useEffect(() => {
     var _mtm = window._mtm = window._mtm || [];
     _mtm.push({ 'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start' });
+
     (function () {
       var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
       g.async = true; g.src = 'https://ec2-3-104-45-196.ap-southeast-2.compute.amazonaws.com/js/container_YbYkzbgl.js'; s.parentNode.insertBefore(g, s);
